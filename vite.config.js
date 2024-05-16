576d8163c17888c973ddb0ae3f18130020c8d256
sawnaytharpoe02 <sawnaytharpoe14@gmail.com>
2024-05-16 16:32:58 +0630

    vite config js edited

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,40 @@
-export default {
+// export default {
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         contact: "./src/pages/contact.html",
+//         signIn: "./src/pages/signIn.html",
+//         signUp: "./src/pages/signUp.html",
+//         forgot: "./src/pages/forgot.html",
+//         about: "./src/pages/about.html",
+//         articles: "./src/pages/articles.html",
+//       },
+//       output: {
+//         format: "esm",
+//         dir: "dist",
+//         entryFileNames: "[name].js",
+//       },
+//     },
+//   },
+// };
+import { defineConfig } from "vite";
+
+export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        contact: './src/pages/contact.html',
-        signIn: './src/pages/signIn.html',
-        signUp: './src/pages/signUp.html',
-        forgot: './src/pages/forgot.html',
-        about: './src/pages/about.html',
-        articles: './src/pages/articles.html',
+        main: "./index.html",
+        contact: "./src/pages/contact.html",
+        signIn: "./src/pages/signIn.html",
+        signUp: "./src/pages/signUp.html",
+        forgot: "./src/pages/forgot.html",
+        about: "./src/pages/about.html",
+        articles: "./src/pages/articles.html",
       },
       output: {
-        format: 'esm',
-        dir: 'dist',
-        entryFileNames: '[name].js'
-      }
-    }
-  }
-}
+        dir: "dist",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
